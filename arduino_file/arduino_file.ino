1a57ddfdd271a0d4fe2d7e86f9c651c874fb9f83
llucero <llucero@fusap.com.ar>
2023-07-10 21:14:30 -0300

    del comentarios, refactor

--- arduino_file/arduino_file.ino
+++ arduino_file/arduino_file.ino
@@ -59,7 +59,10 @@ int pinMovimiento = A5;
 int movimientoDetectado = 0;
 
 
-
+//-------------------Configuracion temporizador-----------------------------------//
+int temporizador = 3;
+unsigned long tiempoInicio; 
+const unsigned long tiempoEspera = 1000;
 
 void setup() {
   // init comunicación serie a 9600 baudios (monitor serial)
@@ -68,15 +71,11 @@ void setup() {
   dht.begin();
   //pinLedR como output
   pinMode(pinLedR, OUTPUT);
-
   //pinled sensor movimiento
   pinMode(pinMovimiento, INPUT);
 }
 
-int temporizador = 3;
 
-unsigned long tiempoInicio;  // Variable para almacenar el tiempo de inicio
-const unsigned long tiempoEspera = 1000;
 
 
 void loop() {
@@ -85,9 +84,6 @@ void loop() {
   key = keypad.getKey();
   activarAlarma(key);
 
-
-
-
   detectarMovimiento();
 
 
@@ -95,7 +91,8 @@ void loop() {
 
   // Si el temporizador ha expirado, realizar una acción
   if (tiempoActual - tiempoInicio >= tiempoEspera) {
-    // Realizar la acción correspondiente
+    // 1 - Si la alarma esta activada y se detecta movimiento se enciende la sirena
+    // 2 - Caso contrato se leen los sensores
     if (alarmaActivada && movimientoDetectado == 1) {
       activarSirena();
     }
@@ -108,7 +105,6 @@ void loop() {
       //--------------------------------Activacion de calefaccion, ventilacion y sirena-----------------------------//
       TEMPERATURA = dht.readTemperature();  // Obtener valor de temperatura
       HUMEDAD = dht.readHumidity();         // Obtener valor de humedad
-      //---------------------------falta agregar el movimiento deberia pasarse a la funcion y condicionarlo segun el TP----------------------------------//
       activarCalefaccionVentilacion(TEMPERATURA, HUMEDAD);
       //---------------------------------------Activacion de calefaccion, ventilacion y sirena-----------------------------------------------//
 
@@ -125,7 +121,6 @@ void loop() {
 
 
 
-      //delay(1500);
     }
 
     tiempoInicio = tiempoActual;  // Reiniciar el temporizador
@@ -176,16 +171,6 @@ void activarAlarma(char key) {
 
 void activarSirena() {
   tone(pinaltavoz, frecuencia);
-  //tone(pinaltavoz,frecuencia) --> activacion altavoz
-  //noTone(pinaltavoz) --> desactivacion altavoz
-  /* crear detectoMoviento
-  if(alarmaActivada && detectoMoviento){
-    tone(pinaltavoz,frecuencia);
-  }
-  else{
-    noTone(pinaltavoz);
-  }
-  */
 }
 void desactivarSirena() {
   noTone(pinaltavoz);
@@ -194,11 +179,7 @@ void desactivarSirena() {
 
 void detectarMovimiento() {
 
-  int value = digitalRead(pinMovimiento);  // lee valor sensor movimiento
-
-
-  if (value == 1) movimientoDetectado = 1;
-  else movimientoDetectado = 0;
+  movimientoDetectado = digitalRead(pinMovimiento);  // lee valor sensor movimiento
 }
 
 
@@ -262,96 +243,3 @@ void activarLed(int ILUMINACION) {
   }
   Serial.print("\r\n");
 }
-
-
-
-
-
-
-
-/*
-
-  #include <DHT.h>		// importa la Librerias DHT
-  #include <DHT_U.h>
-
-  //led
-  int pinLedR = 13; // pin Rojo del LED RGB
-  int pausa = 1000;
-
-  //buzzer
-  int pinaltavoz = 3;
-  int frecuencia=220;    // frecuencia correspondiente a la nota La
-
-  //constantes del sensor de luz
-  const long A = 1000;     //Resistencia en oscuridad en KΩ
-  const int B = 15;        //Resistencia a la luz (10 Lux) en KΩ
-  const int Rc = 10;       //Resistencia calibracion en KΩ
-  const int LDRPin = A3;   //Pin del LDR
-
-  //variables del sensor de luz
-  int V;
-  int ilum;
-
-  //variables del sensor de temperatura
-  int SENSOR = A4;			// pin DATA de DHT22 a pin digital 2
-  int TEMPERATURA;
-  int HUMEDAD;
-
-  DHT dht(SENSOR, DHT11);		// creacion del objeto, cambiar segundo parametro
-				// por DHT11 si se utiliza en lugar del DHT22
-  void setup(){
-  Serial.begin(9600);		// inicializacion de monitor serial
-  dht.begin();			// inicializacion de sensor de temperatura
-  pinMode(pinLedR, OUTPUT); // pone el pinLedR como output
-  }
-
-  void loop(){
-  //sensor de luz
-  V = analogRead(LDRPin);
-  ilum = ((long)V*A*10)/((long)B*Rc*(1024-V));    //usar si LDR entre A0 y Vcc (como en el esquema anterior)
-  Serial.print(" Luz: ");
-  Serial.print(ilum);
-
-  //sensor de temperatura
-  TEMPERATURA = dht.readTemperature();	// obtencion de valor de temperatura
-  HUMEDAD = dht.readHumidity();		// obtencion de valor de humedad
-  Serial.print(" Temperatura: ");	// escritura en monitor serial de los valores
-  Serial.print(TEMPERATURA);
-  Serial.print(" Humedad: ");
-  Serial.println(HUMEDAD);
-
-  //verificacion de nivel de luz
-  if (ilum<100){
-      Serial.println(" Led encendido");
-      analogWrite(pinLedR, 255); //encendido
-  }
-  else
-    analogWrite(pinLedR, 0); //apagado
-
-  //verificacion de niveles de temperatura
-  if (TEMPERATURA >25){
-      Serial.println(" Ventilacion encendida");
-      if (TEMPERATURA == 50) {
-        Serial.println(" Alarma encendida!");
-        //enciendo altavoz
-        tone(pinaltavoz,frecuencia);}
-       else
-        noTone(pinaltavoz);
-  }
-  else{
-    if (TEMPERATURA <5)
-      Serial.println(" Calefaccion encendida");
-  }
-  //verificacion de niveles de humedad
-  if (HUMEDAD >80)
-     Serial.println(" Riego detenido");
-  else
-   if (HUMEDAD <50)
-     Serial.println(" Riego activado");
-
-  Serial.println("");
-
-  delay(1000);
-  }
-
-*/
